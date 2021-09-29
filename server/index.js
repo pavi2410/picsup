2880a472f4e4c1acdd1e2af2467863e72cdf738f
Pavitra Golchha <hello@pavi2410.me>
2021-09-29 23:09:02 +0530

    Day 1: server

--- server/index.js
+++ server/index.js
@@ -10,3 +10,14 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+
+
+// const { MongoClient } = require('mongodb');
+// const uri = "mongodb+srv://Admin:admin@picsup.ifxzn.mongodb.net/picsup?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
