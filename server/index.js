ecf6c9b27c72467e97102ec1370d938c8a8ca9e2
Pavitra Golchha <hello@pavi2410.me>
2021-10-01 23:52:47 +0530

    Day 2: Completed backend

--- server/index.js
+++ server/index.js
@@ -1,23 +1,104 @@
+const os = require('os');
+const path = require('path');
+const fs = require('fs/promises');
+const express = require('express');
+const mongoose = require('mongoose');
+var multer = require('multer');
 
-const express = require('express')
+// ------- Setup -------
+
+mongoose.connect('mongodb+srv://Admin:admin@picsup.ifxzn.mongodb.net/picsup?retryWrites=true&w=majority').
+then(()=>{console.log("DB connected")},(err)=>{console.log(err)});
+
 const app = express()
 const port = 3000
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    const tmpdir = path.join(os.tmpdir(), 'picsup')
+    fs.mkdir(tmpdir, {recursive:true})
+    .then(() => {
+      cb(null, tmpdir)
+    })
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now())
+  }
+})
+
+var upload = multer({ storage: storage })
+
+// ------- Models -------
+
+var imageSchema = new mongoose.Schema({
+  name: String,
+  img:
+  {
+    data: Buffer,
+    contentType: String
+  }
+})
+const Images = new mongoose.model('Image', imageSchema);
+
+// ------- Request handlers -------
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+app.get('/images', (req, res) => {
+  Images.find({})
+  .then((images, err) => {
+    if (err) {
+      console.log('/images:', err)
+      res.sendStatus(404)
+      return
+    }
+    res.set('Content-Type', images[0].img.contentType);
+    res.send(images[0].img.data)
+  })
 })
 
+app.get('/image/:id', (req, res) => {
+  // console.log({params: req.params})
+  Images.findById(req.params.id).then( (image, err) => {
+    if (err) {
+      res.sendStatus(404);
+      return;
+    }
 
+    res.set('Content-Type', image.img.contentType)
+    res.send(image.img.data)
+  })
+})
 
-// const { MongoClient } = require('mongodb');
-// const uri = "mongodb+srv://Admin:admin@picsup.ifxzn.mongodb.net/picsup?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   client.close();
-// });
+app.post('/upload', upload.single('uploaded_file'), function (req, res) {
+  const file = req.file;
+  if(!file){
+    res.status(400).send("Please upload a file");
+  }
+
+  fs.readFile(req.file.path).then(file => {
+    console.log('upload', file)
+    var obj = {
+      name: req.file.originalname,
+      img: {
+        data: file,
+        contentType: req.file.mimetype
+      }
+    }
+    const newimage = new Images(obj);
+    newimage.save(obj, (err, resp) => {
+      if (err) {
+        res.status(500).send('Failed to store image')
+        return
+      }
+      console.log(resp);
+      res.send("id: " + resp._id)
+    });
+  })
+});
+
+app.listen(port, () => {
+  console.log(`picsup server listening at http://localhost:${port}`)
+})
