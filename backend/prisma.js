d752c752674011772db6e5c9d4b6132b209523e6
Pavitra Golchha <hello@pavi2410.me>
2022-01-16 18:20:14 +0530

    Add logging middleware to prisma client

--- backend/prisma.js
+++ backend/prisma.js
@@ -2,6 +2,18 @@ import prismaModule from '@prisma/client'
 
 const { PrismaClient } = prismaModule
 
-let prisma = new PrismaClient()
+const prisma = new PrismaClient()
+
+prisma.$use(async (params, next) => {
+  const before = Date.now()
+
+  const result = await next(params)
+
+  const after = Date.now()
+
+  console.log(`Query ${params.model}.${params.action} took ${after - before}ms`)
+
+  return result
+})
 
 export default prisma
