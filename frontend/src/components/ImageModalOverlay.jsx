3cacb7916b042382c33fbac09a3f9ae1a4cd2eb7
Pavitra Golchha <hello@pavi2410.me>
2022-01-16 22:48:39 +0530

    fix image modal

--- frontend/src/components/ImageModalOverlay.jsx
+++ frontend/src/components/ImageModalOverlay.jsx
@@ -4,7 +4,7 @@ export default function ImageModalOverlay({ images, idx, setOpenImageModal, setI
     return (
       <div className="fixed left-0 top-0 z-10 overflow-y-hidden w-full h-full flex justify-center align-center bg-green-200 backdrop-filter backdrop-blur-3xl bg-opacity-50">
         <div className="m-auto">
-          <img src={`${HOST}/image/${images[idx]}`} className="sm:w-screen sm:h-auto md:w-auto md:h-96" />
+          <img src={`${HOST}/images/image/${images[idx]}`} className="sm:w-screen sm:h-auto md:w-auto md:h-96" />
           <div className="flex items-center justify-center pt-6 border-t border-solid border-blueGray-200 rounded-b">
             <button
               className="text-black background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1"
