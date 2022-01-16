13fb95625f54b7f78b5852b22f8969e968f41746
Pavitra Golchha <hello@pavi2410.me>
2022-01-16 18:20:36 +0530

    Update imageController to use prisma

--- backend/routers/images/imageController.js
+++ backend/routers/images/imageController.js
@@ -4,33 +4,29 @@ import prisma from '../../prisma.js'
 const upload = multer()
 
 export const getAllImages = async (req, res) => {
-  Images.find({})
-    .then((images, err) => {
-      if (err) {
-        console.log('/images:', err)
-        res.sendStatus(404)
-        return
-      };
-      res.json({ images: images.map(image => image._id) })
-    })
+  const images = await prisma.images.findMany()
+
+  res.json({ images: images.map(image => image.id) })
 }
 
-export const getImageById = (req, res) => {
-  // console.log({params: req.params})
-  Images.findById(req.params.id).then((image, err) => {
-    if (err || image == null) {
-      res.sendStatus(404);
-      return;
+export const getImageById = async (req, res) => {
+  const { id: imageId } = req.params
+
+  const image = await prisma.images.findUnique({
+    where: {
+      id: imageId
     }
-    res.set('Content-Type', image.img.contentType)
-    res.send(image.img.data)
   })
+
+  res.set('Content-Type', image.img.contentType)
+  res.send(image.img.data)
 }
 
 export const uploadSingleFile = upload.single('uploaded_file')
 
 export const uploadImage = async (req, res) => {
   const file = req.file;
+
   if (!file) {
     res.status(400).send("Please upload a file");
     return
@@ -40,59 +36,54 @@ export const uploadImage = async (req, res) => {
 
   const tags = [...req.body.tags.split(',')]
 
-  const obj = {
-    name: file.originalname,
-    img: {
-      data: file.buffer,
-      contentType: file.mimetype
-    },
-    ownerid: req.user._id,
-    tags: tags,
-  }
-  const newimage = new Images(obj);
-  newimage.save((err, resp) => {
-    if (err) {
-      res.status(500).send('Failed to store image')
-      return
+  const newimage = await prisma.images.create({
+    data: {
+      name: file.originalname,
+      img: {
+        data: file.buffer,
+        contentType: file.mimetype
+      },
+      ownerid: req.user._id,
+      tags: tags,
     }
-    res.json({ id: resp._id })
   });
+
+  res.json({ id: newimage.id })
 }
 
 export const deleteImageById = async (req, res) => {
   const { id } = req.params
 
-  await prisma.
-
-    Images.findOneAndRemove({ _id: req.params.id, ownerid: req.user._id }).then((image, err) => {
-      if (err || image == null) {
-        res.sendStatus(404);
-        return;
-      }
-      res.send("Image Deleted");
-    })
-}
-
-export const getAllOwnerImages = (req, res) => {
-  Images.find({ ownerid: req.user._id })
-    .then((images, err) => {
-      if (err) {
-        console.log('/images:', err)
-        res.sendStatus(404)
-        return
-      };
-      res.json({ images: images.map(image => image._id) })
-    })
-}
-
-export const getOwnerImageById = (req, res) => {
-  // console.log({params: req.params})
-  Images.findById(req.params.id).then((image, err) => {
-    if (err || image == null) {
-      res.sendStatus(404);
-      return;
+  await prisma.images.delete({
+    where: {
+      id: id,
+      ownerid: req.user._id
     }
-    res.set('Content-Type', image.img.contentType)
-    res.send(image.img.data)
   })
+
+  res.send("Image Deleted");
+}
+
+export const getAllOwnerImages = async (req, res) => {
+  const images = await prisma.images.findMany({
+    where: {
+      ownerid: req.user._id
+    }
+  })
+
+  res.json({ images: images.map(image => image.id) })
+}
+
+export const getOwnerImageById = async (req, res) => {
+  const { id: imageId } = req.params
+
+  const image = await prisma.images.findUnique({
+    where: {
+      id: imageId,
+      ownerid: req.user._id
+    }
+  })
+
+  res.set('Content-Type', image.img.contentType)
+  res.send(image.img.data)
 }
