7c956f0c88c4b2f979a1e77aac5b38434c004024
Pavitra Golchha <hello@pavi2410.me>
2022-09-10 05:29:58 +0530

    React 18

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client';
 import "tailwindcss/tailwind.css"
 import "@fontsource/poppins"; // 400
 import App from './App'
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+  </React.StrictMode>
+);
